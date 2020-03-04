6b88dc5fe83553b356f6c22187668302b0b4f48d
Andre Skeene <halodre24@gmail.com>
2020-03-04 23:25:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,12 @@
 class CashRegister
-  attr_reader :discount
+  attr_reader :discount, :total
 
   def initialize(discount_amount = 0)
     @total = 0
     @discount = discount_amount
   end
 
-  def total(num)
+  def total=(num)
     @total = num
   end
 end
